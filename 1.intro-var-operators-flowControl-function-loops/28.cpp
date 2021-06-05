2046c80b8dd3b5171f7db4487a406e9e0f9141ff
karth <karthiknaveen22022002@gmail.com>
2021-06-05 23:11:03 +0530

    com at 11

--- 1.intro-var-operators-flowControl-function-loops/28.cpp
+++ 1.intro-var-operators-flowControl-function-loops/28.cpp
@@ -1,9 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// printing each digit in a number in a number using recursion
+// and printing each digit number in a number using recursion
+
+void printEachNum(int n){
+	if (n==0){
+		cout<<0;
+		return;
+	}
+	int r=n%10;
+	// cout<<r<<endl;
+	printEachNum(n/10);
+	cout<<r<<endl;
+}
+
 int main (){
-	int i=34;
-	string s=i;
-	cout<<s;
+	int n=3234;
+	int i=0;
+	while (n>0){
+		cout<<n%10;
+		n=n/10;
+
+
+
+	}
+
+
+
+
+	// printEachNum(n);
 	
 	return 0;
 }
