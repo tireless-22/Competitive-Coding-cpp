9907e45fed72fca9cf2df8c66da2b413b49cd76e
karth <karthiknaveen22022002@gmail.com>
2021-06-06 23:02:38 +0530

    commit at 11 pm of 6th june

--- 2.arrays-string-pointers-reference-structures-unions/6.cpp
+++ 2.arrays-string-pointers-reference-structures-unions/6.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main (){
+	// REFERENCES
+
+	// reference means it is not 
+	// copying ,it is just like giving a
+	// name to the existing variable
+
+	// if we change the original the refrenced will change
+	// and vise versa
+	int x=32;
+	int &y=x;
+	y+=5;
+	cout<<x<<endl;
+	x+=2;
+	cout<<y<<endl;
+
+
 	
 	return 0;
 }
--- 2.arrays-string-pointers-reference-structures-unions/7.cpp
+++ 2.arrays-string-pointers-reference-structures-unions/7.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
+// void swap(int x,int y){
+// 	// here this is normal function
+// 	// changes that are made here are not get reflected 
+// 	// even though we use the same variable or different variable
+// 	int temp=y;
+// 	y=x*2;
+// 	x=temp*2;
+// }
+
+
+void swap(int &r,int &t){
+
+	// here we referenced the x with r and y with t
+	// we can also reference the x with x and y with y
+	// so the changes made in this method get reflected to the x and y
+	int temp=t;
+	t=r;
+	r=temp;
+}
 int main (){
-	
+	int x=3;
+	int y=34;
+	swap(x,y);
+	cout<<x<<" "<<y;
 	return 0;
 }
