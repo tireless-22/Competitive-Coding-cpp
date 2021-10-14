a75f1c3cc3b87953edb7e2f14e8b7ee412bce38d
karth <karthiknaveen22022002@gmail.com>
2021-10-14 23:20:13 +0530

    added dp solutions

---
+++ DATASTRUCTURES AND ALGOS/34.DP/22-MinCostClimbingStairs(leetCode)Recursive.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int mini(vector<int> v,int n){
+	if(n==0 or n==1){
+		return 0;
+	}
+
+	// if(n==1){
+	// 	return min({v[0],v[1]});
+	// }
+
+	int firstMin=mini(v,n-1)+v[n-1];
+	int secondMin=mini(v,n-2)+v[n-2];
+
+	return min({firstMin,secondMin});
+
+}
+
+int main(){
+
+	vector<int> v={1,100,1,1,1,100,1,1,100,1};
+	int n=v.size();
+
+	cout<<mini(v,n);
+
+
+
+
+
+
+	return 0;
+}
--- DATASTRUCTURES AND ALGOS/9.LinkedList/3-traversalOfLinkedList.cpp
+++ DATASTRUCTURES AND ALGOS/9.LinkedList/3-traversalOfLinkedList.cpp
@@ -1,20 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 struct Node{
 	// only to create the nodes
 	int data;
 	Node *next;
-
-
 	Node(int x){
 		data=x;
 		next=NULL;
 	}
-
 };
-
 // to print the elements in the linked list
 
 void printLl(Node *head){
@@ -28,14 +23,28 @@ void printLl(Node *head){
 	
 }
 
+
 int main(){
 	Node *head=new Node(5);
 	Node *n1=new Node(3);
 	Node *n2=new Node(2);
+	Node *n3=new Node(1);
 	head->next=n1;
 	n1->next=n2;
+	n2->next=n3;
+
+
+
+
+
 	printLl(head);
+	cout<<endl;
+
+	head=reverse(head);
+
 	printLl(head);
-	printLl(head);
+
+
+
 	return 0;
 }
